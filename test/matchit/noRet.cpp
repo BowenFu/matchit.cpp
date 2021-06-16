a7191b45a2dc6a5ef46bac4ed993735c66830d1b
Bowen Fu <bowen.full@gmail.com>
2021-06-16 22:43:15 +0800

    Fix one more coverage hole.

--- test/matchit/noRet.cpp
+++ test/matchit/noRet.cpp
@@ -8,7 +8,7 @@ TEST(MatchStatement, test)
 {
   testing::internal::CaptureStdout();
   match(4)(
-      pattern(1) = [] { std::cout << "mismatch!"; },
+      pattern(or_(_ < 0, 2)) = [] { std::cout << "mismatch!"; },
       pattern(_) = [] { std::cout << "match all!"; });
   std::string output = testing::internal::GetCapturedStdout();
 
