62d637eccfc5db2fa6dd95c5d262fd8babb34f38
Bowen Fu <missing>
2021-06-25 22:31:59 +0800

    Add samples.

--- samples/terminate.cpp
+++ samples/terminate.cpp
@@ -4,6 +4,7 @@
 enum class Op { Add, Sub, Mul, Div };
 
 Op parseOp(char t) {
+    using namespace matchit;
     Id<char> token;
     return match(t) ( 
         pattern | '+' = expr(Op::Add),
@@ -13,12 +14,13 @@ Op parseOp(char t) {
         pattern | token = [&]{
             std::cerr << "Unexpected: " << *token;
             std::terminate();
+            return Op::Add;
         }
     );
 }
 
 int32_t main()
 {
-    std::cout << static_cast<int>(parseOp('*')) << std::endl;
+    std::cout << static_cast<int>(parseOp('/')) << std::endl;
     return 0;
 }
