5bd8b167bf1fc72c76407ceee3a0f43f4515244a
Bowen Fu <missing>
2021-10-23 09:43:24 +0800

    Add slides.

--- sample/Red-black-Tree-Rebalancing.cpp
+++ sample/Red-black-Tree-Rebalancing.cpp
@@ -28,6 +28,7 @@ bool operator==(Node<T> const &lhs, Node<T> const &rhs)
          lhs.value == rhs.value && lhs.rhs == rhs.rhs;
 }
 
+#if 0
 template <typename T>
 void Node<T>::balance()
 {
@@ -80,6 +81,55 @@ void Node<T>::balance()
       pattern | self = expr(self) // do nothing
   );
 }
+
+#else
+
+template <typename T>
+void Node<T>::balance()
+{
+  using namespace matchit;
+
+  constexpr auto dsN = [](auto &&color, auto &&lhs, auto &&value, auto &&rhs)
+  {
+    return and_(app(&Node<T>::color, color), app(&Node<T>::lhs, lhs),
+                app(&Node<T>::value, value), app(&Node<T>::rhs, rhs));
+  };
+
+  constexpr auto blackN = [dsN](auto &&lhs, auto &&value, auto &&rhs)
+  {
+    return dsN(Black, lhs, value, rhs);
+  };
+
+  constexpr auto redN = [dsN](auto &&lhs, auto &&value, auto &&rhs)
+  {
+    return dsN(Red, lhs, value, rhs);
+  };
+
+  Id<std::shared_ptr<Node<T>>> a, b, c, d;
+  Id<T> x, y, z;
+  Id<Node> self;
+  *this = match(*this)(
+      pattern | blackN(some(redN(some(redN(a, x, b)), y, c)), z, d) // left-left case
+      = [&]
+      { return Node{Red, std::make_shared<Node>(Black, *a, *x, *b), *y,
+                    std::make_shared<Node>(Black, *c, *z, *d)}; },
+      pattern | blackN(some(redN(a, x, some(redN(b, y, c)))), z, d) // left-right case
+      = [&]
+      { return Node{Red, std::make_shared<Node>(Black, *a, *x, *b), *y,
+                    std::make_shared<Node>(Black, *c, *z, *d)}; },
+      pattern | blackN(a, x, some(redN(some(redN(b, y, c)), z, d))) // right-left case
+      = [&]
+      { return Node{Red, std::make_shared<Node>(Black, *a, *x, *b), *y,
+                    std::make_shared<Node>(Black, *c, *z, *d)}; },
+      pattern | blackN(a, x, some(redN(b, y, some(redN(c, z, d))))) // right-right case
+      = [&]
+      { return Node{Red, std::make_shared<Node>(Black, *a, *x, *b), *y,
+                    std::make_shared<Node>(Black, *c, *z, *d)}; },
+      pattern | self = expr(self) // do nothing
+  );
+}
+
+#endif
 
 int main()
 {
